50ac2bf4c81bf2e8923d2db2d7da259f694d69b6
TasbasMN <nazifts@gmail.com>
2023-09-22 20:14:37 +0300

    add step 5 of xgb pipeline

--- scripts/globals.py
+++ scripts/globals.py
@@ -11,4 +11,7 @@ def get_RNAduplex_location():
 RNADUPLEX_LOCATION = get_RNAduplex_location()
 PYENSEMBL_CACHE_DIR = "/run/media/nazif/2F946E411BA61D49/data"
 XGB_PIPELINE_RESULTS = "scripts/xgb_pipeline/results"
+
 CLASH_CSV = "data/clash/clash_parsed.csv"
+TA_SPS_CSV = "data/ta_sps/ta_sps.csv"
+MIRNA_CSV = "data/mirna/mirna.csv"
